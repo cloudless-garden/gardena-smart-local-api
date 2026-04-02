9b66fcae1e68183540f0d32928334de9e1a670f7
Ezra Buehler <ezra.buehler@husqvarnagroup.com>
2026-04-02 11:52:29 +0200

    Add live update support to sensor example
    To watch the sensor values for 60 seconds, run:
    
        uv run gardena_smart_local_api/examples/sensor.py -g $GW -p $PW \
            -d $DEVICE_ID read 60

--- gardena_smart_local_api/examples/sensor.py
+++ gardena_smart_local_api/examples/sensor.py
@@ -2,6 +2,9 @@
 import asyncio
 import sys
 
+from rich.live import Live
+from rich.text import Text
+
 from gardena_smart_local_api.devices import Sensor1, Sensor2
 from gardena_smart_local_api.examples import ExampleApp
 
@@ -15,6 +18,13 @@ async def main():
             "nargs": 1,
             "choices": ("list", "read"),
             "help": "List applicable devices or read sensor values",
+        },
+        {
+            "name_or_flags": ["duration"],
+            "nargs": "?",
+            "default": 1,
+            "type": int,
+            "help": "Duration to watch the sensor values (default 1s)",
         },
     ]
 
@@ -31,15 +41,41 @@ async def main():
                 if not isinstance(sensor, COMPATIBLE):
                     print("Incompatible device selected")
                     return 1
-                print(f"Sensor {sensor.id}:")
-                print(f"  Temperature:     {sensor.temperature}°C")
-                print(f"  Soil moisture:   {sensor.soil_moisture}%")
-                if isinstance(sensor, Sensor1):
-                    print(f"  Light:           {sensor.light} lx")
-                print(f"  Battery:         {sensor.battery_level}%")
-                print(f"  RF link quality: {sensor.rf_link_quality}%")
-                print(f"  Frost warning:   {sensor.has_frost_warning}")
-                print(f"  Error:           {sensor.error}")
+                try:
+                    await asyncio.wait_for(
+                        _display(app, sensor), timeout=app.args.duration
+                    )
+                except TimeoutError:
+                    pass
+
+
+async def _display(app, sensor):
+    with Live(auto_refresh=False) as live:
+        while True:
+            out = f"Sensor {sensor.id}:\n"
+            out += f"  Temperature:     {sensor.temperature}°C\n"
+            out += f"  Soil moisture:   {sensor.soil_moisture}%\n"
+            if isinstance(sensor, Sensor1):
+                out += f"  Light:           {sensor.light}lx\n"
+            out += f"  Battery:         {sensor.battery_level}%\n"
+            out += f"  RF link quality: {sensor.rf_link_quality}%\n"
+            out += f"  Frost warning:   {sensor.has_frost_warning}\n"
+            out += f"  Error:           {sensor.error}"
+
+            live.update(Text(out))
+            live.refresh()
+
+            request = sensor.build_refresh_temperature_obj()
+            await app.send_request(request)
+            request = sensor.build_refresh_soil_moisture_obj()
+            await app.send_request(request)
+            if isinstance(sensor, Sensor1):
+                request = sensor.build_refresh_light_obj()
+                await app.send_request(request)
+            request = sensor.build_refresh_battery_level_obj()
+            await app.send_request(request)
+            request = sensor.build_refresh_rf_link_quality_obj()
+            await app.send_request(request)
 
 
 if __name__ == "__main__":

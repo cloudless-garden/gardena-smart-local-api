201e063fb9e1879219cdae3fa209b1343ceb56b1
Florian Schweikert <florian.schweikert@husqvarnagroup.com>
2026-04-29 17:44:11 +0200

    Rename Sgtin96Info to SGTIN96Info to comply with PEP 8

--- tests/test_sgtin96.py
+++ tests/test_sgtin96.py
@@ -1,6 +1,6 @@
 import pytest
 
-from gardena_smart_local_api.sgtin96 import Sgtin96Info
+from gardena_smart_local_api.sgtin96 import SGTIN96Info
 
 
 @pytest.mark.parametrize(
@@ -16,22 +16,22 @@ from gardena_smart_local_api.sgtin96 import Sgtin96Info
 )
 @pytest.mark.asyncio
 async def test_parse_sgtin96_model_name(hex_str, model_name):
-    assert await Sgtin96Info.from_hex(hex_str).get_model_name() == model_name
+    assert await SGTIN96Info.from_hex(hex_str).get_model_name() == model_name
 
 
 def test_parse_sgtin96_invalid_length():
     with pytest.raises(ValueError, match="24 hex characters"):
-        Sgtin96Info.from_hex("3034F8EE")
+        SGTIN96Info.from_hex("3034F8EE")
 
 
 def test_parse_sgtin96_invalid_header():
     with pytest.raises(ValueError, match="invalid header"):
-        Sgtin96Info.from_hex("FF34F8EE901267400000B333")
+        SGTIN96Info.from_hex("FF34F8EE901267400000B333")
 
 
 def test_parse_sgtin96_valid_hex():
     hex_str = "3034F8EE901267400000B333"
-    info = Sgtin96Info.from_hex(hex_str)
+    info = SGTIN96Info.from_hex(hex_str)
     assert info.serial == 45875
     assert info.company_prefix == "4078500"
     assert info.item_reference == "18845"
